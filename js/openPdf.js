751695dc5acf1b17dbe9e88c258581ebdd2bfccc
mmmmatsuuuu <matsumura-t-mi@g.bears.ed.jp>
2022-02-16 20:21:13 +0900

    pdfを開く機能及びタグクリックでの検索機能追加

--- js/openPdf.js
+++
@@ -1,8 +0,0 @@
-function openPdf(base64str) {
-    const str = String(base64str).split("data:application/pdf;base64,")[1];
-    const file = new Blob([atob(str)], {
-        type: 'application/pdf'
-    });
-    const fileURL = URL.createObjectURL(file);
-    window.open(fileURL);
-}
